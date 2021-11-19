10dd3970a50b6d019b5c83902f4b53fa3daed87a
Aleksi Viitanen <n1vial00@students.oamk.fi>
2021-11-19 16:05:30 +0200

    Matikkakoodi jatkoa. Ei toimi tällä hetkellä.

--- js/code-matikka.js
+++ js/code-matikka.js
@@ -1,21 +1,4 @@
-
-
-class Lasku {
-/**
- * 
- * @param {Number} laskujenM Kuinka monta laskua sivulle tehdään
- * @param {Number} lukujenM Kuinta monta lukua per lasku
- * @param {Boolean} plus Onko laskuissa plusmerkkejä?
- * @param {Boolean} miinus Onko laskuissa miinusmerkkejä?
- * @param {Boolean} kerto Onko laskuissa kertomamerkkejä?
- */
-    constructor(lukujenM, plus, miinus, kerto) {
-        this.lukujenM = lukujenM;
-        this.plus = plus;
-        this.miinus = miinus;
-        this.kerto = kerto;
-    }
-}
+let kaikkiLaskut = new Array;
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
@@ -24,23 +7,47 @@ function getRndInteger(min, max) {
 // Tämä funktio lisää sattumanvaraisen numeron laskuihin
 function vaihdaMatikka() {
     let allNumbers = document.querySelectorAll(".changingNR");
-    let vaihteluMin = Number(document.querySelector("#vaihteluMin").value);
-    let vaihteluMax = Number(document.querySelector("#vaihteluMax").value);
+
     for(i = 0; i < allNumbers.length; i++) {
         let randomNumber = getRndInteger(vaihteluMin,vaihteluMax);
         allNumbers[i].textContent = randomNumber;
     }
 }
 
-// Tämä funktio tarkistaa ovatko käyttäjän antamat vastaukset oikein
-function matikkaTarkistus()  {
-    let number1 = document.querySelector("#addNumber1")
-    let number2 = document.querySelector("#addNumber2")
+
+function matikkaToteutus()  {
+    let div = document.createElement("div");
+
+    let laskuM = document.querySelector("#laskuM");
+    let lukujenM = document.querySelector("#lukujenM");
+    let vaihteluMin = Number(document.querySelector("#vaihteluMin").value);
+    let vaihteluMax = Number(document.querySelector("#vaihteluMax").value);
+    let plus = document.querySelector("#plusBox").checked;
+    let miinus = document.querySelector("#miinusBox").checked;
+    let kerto = document.querySelector("#kertoBox").checked;
+    let vMerkit = [];
     
-    
+    if(plus) {
+        vMerkit.push("+");
+    }
+    if(miinus) {
+        vMerkit.push("-");
+    }
+    if(kerto) {
+        vMerkit.push("*");
+    }
+
+    for(i = 0; i < laskuM.value; i++) {
+        let randomLuku = getRndInteger(0, lukujenM - 1);
+        let lukuJono = getRndInteger(vaihteluMin, vaihteluMax);
+        for(j = 0; j < randomLuku; j++) {
+            lukuJono += vMerkit[getRndInteger(0, vMerkit.length)] + " " + getRndInteger(vaihteluMin, vaihteluMax);
+        }
+        kaikkiLaskut.push(lukuJono);
+    }
+
 }
 
 
-document.querySelector("#addButton").addEventListener("click",matikkaTarkistus)
-document.querySelector("#vaihdaMatikkaButton").addEventListener("click",vaihdaMatikka);
+document.querySelector("#teeLaskut").addEventListener("click",matikkaToteutus)
 
