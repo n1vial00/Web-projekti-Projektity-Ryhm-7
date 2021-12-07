188385c707b7da19f88853d576c402049d7ef70e
Kortesalmi <n0koju01@students.oamk.fi>
2021-12-07 14:57:29 +0200

    js

--- js/code-bilsa.js
+++ js/code-bilsa.js
@@ -1,10 +1,14 @@
-let buttons = document.getElementsByClassName("btn btn-success");
-for (let i = 0; i < buttons.length; i++) {
-  buttons[i].onclick = guessResult;
+let etana = document.getElementById('etana');
+
+for (let i = 0; i < etana.length; i++) {
+  etana[i].onclick = guessEtana;
 }
 
-function guessResult() {
-    let incorrect = document.getElementById('incorrect');
-    let etana = incorrect.textContent
-    console.log(etana);
+function guessEtana() {
+  let etana = document.getElementById('etana');
+  if (etana.textContent == 'Etana') {
+      document.getElementById('result').innerHTML = 'Oikein!'
+  } else {
+      document.getElementById('result').innerHTML = 'Väärin!'
+  }
 }
