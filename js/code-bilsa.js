3564e602a8206148d8fd6858608f614ca4f1c500
Kortesalmi <n0koju01@students.oamk.fi>
2021-12-07 16:42:49 +0200

    .

--- js/code-bilsa.js
+++ js/code-bilsa.js
@@ -1,14 +1,17 @@
-function etanaT() {
-  document.getElementById('koira').addEventListener('click', guessEtana)
-  document.getElementById('etana').addEventListener('click', guessEtana)
-  document.getElementById('kirahvi').addEventListener('click', guessEtana)
-  document.getElementById('karpanen').addEventListener('click', guessEtana)
+document.getElementById('koira').addEventListener('click', guessEtana)
+document.getElementById('etana').addEventListener('click', guessEtana)
+document.getElementById('kirahvi').addEventListener('click', guessEtana)
+document.getElementById('karpanen').addEventListener('click', guessEtana)
 
-  let etana = document.getElementById('etana')
+let etana = document.getElementById('etana')
+let koira = document.getElementById('koira')
+let kirahvi = document.getElementById('kirahvi')
+let karpanen = document.getElementById('karpanen')
 
-  function guessEtana() {
-    if (etana) {
-      etana.classList.add()
-    }
+function guessEtana() {
+  if (etana) {
+    console.log('oikein');
+  } else if(koira){
+    console.log('väärin');
   }
 }
