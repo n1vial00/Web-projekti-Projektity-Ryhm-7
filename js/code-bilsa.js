f2aba817e2cc6f5a44e2f7999fa31084660dad79
Kortesalmi <n0koju01@students.oamk.fi>
2021-12-16 13:25:29 +0200

    Lisäsin kasvipuolta

--- js/code-bilsa.js
+++ js/code-bilsa.js
@@ -1,12 +1,29 @@
+// Eläinten tulosten tyhjennys
+document.getElementById('result1').innerHTML = '';
+document.getElementById('result2').innerHTML = '';
+document.getElementById('result3').innerHTML = '';
+document.getElementById('result4').innerHTML = '';
+document.getElementById('result5').innerHTML = '';
+
+// Kasvien tulosten tyhjennys
+document.getElementById('result6').innerHTML = '';
+document.getElementById('result7').innerHTML = '';
+document.getElementById('result8').innerHTML = '';
+document.getElementById('result9').innerHTML = '';
+document.getElementById('result0').innerHTML = '';
+
+// Eläin tehtävien funktiot, joissa tarkistetaan vastaus
+// Jos vastaus oikein isolla tai pienellä alkukirjaimella, tulosta Oikein!
+// Jos jotain muuta, tulosta Väärin!
 function checkEtana() {
   let etana = document.getElementById('etana').value;
 
   if (etana == 'Etana') {
-    console.log('Oikein!');
+    document.getElementById('result1').innerHTML = 'Oikein!';
   } else if (etana == 'etana') {
-    console.log('Oikein!');
+    document.getElementById('result1').innerHTML = 'Oikein!';
   } else {
-    console.log('Väärin!');
+    document.getElementById('result1').innerHTML = 'Väärin!';
   }
 }
 
@@ -14,11 +31,11 @@ function checkKoira() {
   let koira = document.getElementById('koira').value;
 
   if (koira == 'Koira') {
-    console.log('Oikein!');
+    document.getElementById('result2').innerHTML = 'Oikein!';
   } else if (koira == 'koira') {
-    console.log('Oikein!');
+    document.getElementById('result2').innerHTML = 'Oikein!';
   } else {
-    console.log('Väärin!');
+    document.getElementById('result2').innerHTML = 'Väärin!';
   }
 }
 
@@ -26,11 +43,11 @@ function checkKani() {
   let kani = document.getElementById('kani').value;
 
   if (kani == 'Kani') {
-    console.log('Oikein!');
+    document.getElementById('result3').innerHTML = 'Oikein!';
   } else if (kani == 'kani') {
-    console.log('Oikein!');
+    document.getElementById('result3').innerHTML = 'Oikein!';
   } else {
-    console.log('Väärin!');
+    document.getElementById('result3').innerHTML = 'Väärin!';
   }
 }
 
@@ -38,11 +55,11 @@ function checkKirahvi() {
   let kirahvi = document.getElementById('kirahvi').value;
 
   if (kirahvi == 'Kirahvi') {
-    console.log('Oikein!');
+    document.getElementById('result4').innerHTML = 'Oikein!';
   } else if (kirahvi == 'kirahvi') {
-    console.log('Oikein!');
+    document.getElementById('result4').innerHTML = 'Oikein!';
   } else {
-    console.log('Väärin!');
+    document.getElementById('result4').innerHTML = 'Väärin!';
   }
 }
 
@@ -50,10 +67,73 @@ function checkTiikeri() {
   let tiikeri = document.getElementById('tiikeri').value;
 
   if (tiikeri == 'Tiikeri') {
-    console.log('Oikein!');
+    document.getElementById('result5').innerHTML = 'Oikein!';
   } else if (tiikeri == 'tiikeri') {
-    console.log('Oikein!');
+    document.getElementById('result5').innerHTML = 'Oikein!';
   } else {
-    console.log('Väärin!');
+    document.getElementById('result5').innerHTML = 'Väärin!';
+  }
+}
+
+// Kasvi tehtävien funktiot, joissa tarkistetaan vastaus
+// Jos vastaus oikein isolla tai pienellä alkukirjaimella, tulosta Oikein!
+// Jos jotain muuta, tulosta Väärin!
+function checkSunflower() {
+  let kukka = document.getElementById('sunflower').value;
+
+  if (kukka == 'Auringonkukka') {
+    document.getElementById('result6').innerHTML = 'Oikein!';
+  } else if (kukka == 'auringonkukka') {
+    document.getElementById('result6').innerHTML = 'Oikein!';
+  } else {
+    document.getElementById('result6').innerHTML = 'Väärin!';
+  }
+}
+
+function checkMansikka() {
+  let mansikka = document.getElementById('mansikka').value;
+
+  if (mansikka == 'Mansikka') {
+    document.getElementById('result7').innerHTML = 'Oikein!';
+  } else if (mansikka == 'mansikka') {
+    document.getElementById('result7').innerHTML = 'Oikein!';
+  } else {
+    document.getElementById('result7').innerHTML = 'Väärin!';
+  }
+}
+
+function checkPeruna() {
+  let peruna = document.getElementById('peruna').value;
+
+  if (peruna == 'Peruna') {
+    document.getElementById('result8').innerHTML = 'Oikein!';
+  } else if (peruna == 'peruna') {
+    document.getElementById('result8').innerHTML = 'Oikein!';
+  } else {
+    document.getElementById('result8').innerHTML = 'Väärin!';
+  }
+}
+
+function checkKaktus() {
+  let kaktus = document.getElementById('kaktus').value;
+
+  if (kaktus == 'Kaktus') {
+    document.getElementById('result9').innerHTML = 'Oikein!';
+  } else if (kaktus == 'kaktus') {
+    document.getElementById('result9').innerHTML = 'Oikein!';
+  } else {
+    document.getElementById('result9').innerHTML = 'Väärin!';
+  }
+}
+
+function checkPalmu() {
+  let palmu = document.getElementById('palmu').value;
+
+  if (palmu == 'Palmu') {
+    document.getElementById('result0').innerHTML = 'Oikein!';
+  } else if (palmu == 'palmu') {
+    document.getElementById('result0').innerHTML = 'Oikein!';
+  } else {
+    document.getElementById('result0').innerHTML = 'Väärin!';
   }
 }
